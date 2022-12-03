619f8c60b703298fe16b12c672c2becc9e0722d6
Jetce <116926177+JetceMx@users.noreply.github.com>
2022-12-02 21:16:15 -0600

    Ya va mejor , pero falta corregir formulario

--- php/Contactanos.php
+++ php/Contactanos.php
@@ -72,7 +72,11 @@
     </script>
     
     
-    <div class="Contactanos">
+    
+    
+    <table>
+       
+     <td>  <div class="Contactanos">
       
         <p >gamessabinito@gmail.com</p>
        
@@ -83,8 +87,69 @@ Mexico.</pre>
         <iframe class="Mapa" src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d246.8572676703629!2d-102.29617696647885!3d21.883383318677833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e0!4m0!4m5!1s0x8429ee63a0cec445%3A0x5fdaa3a7019333ae!2sLic.%20Benito%20Ju%C3%A1rez%2C%20Zona%20Centro%2C%2020000%20Aguascalientes%2C%20Ags.!3m2!1d21.8833888!2d-102.2961897!5e0!3m2!1ses-419!2smx!4v1670027078668!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         
         
+    </div>
+        </td>
+        
+        
+        
+        <td>
+        
+        <div class="Formulario">
+        
+        <form action="">
+            
+                <table>
+                   
+                   <tr>
+                       <td>Nombre</td>
+                       <td>Apellido</td>
+                   </tr>
+                   
+                   <tr>
+                       <td><input type="text" required></td>
+                       <td><input type="text" required></td>
+                   </tr>
+                   
+                   <tr>
+                       <td>Email</td>
+                       <td>Telefono</td>
+                   </tr>
+                   
+                   <tr>
+                       <td><input type="text" required></td>
+                       <td><input type="text"  required></td>
+                   </tr>
+                   
+                   <tr>
+                       <td>Dejanos un Mensaje</td>
+                   </tr>
+                   
+                   <tr>
+                       <td colspan="2"><textarea rows="8" cols="43"></textarea></td>
+                    
+                   <tr>
+                       
+                       <td colspan="2"><input type="submit" class="Boton" ></td>
+                       
+                   </tr>
+                     
+                   
+                   
+                    
+                </table>
+         
+            
+            
+            
+            
+        </form>
         
     </div>
+       </td>
+        
+    </table>
+    
+    
     
     
     
