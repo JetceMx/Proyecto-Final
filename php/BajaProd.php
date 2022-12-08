ba59aed4a86e6881d0bce1f5f984bfbde40428bc
Deivis <davidesparza9090@gmail.com>
2022-12-08 17:01:41 -0600

    Muestra de Productos en BajaProd...
    Ahora cuando se despliega el PHP de BajaProd, se muestra una tabla de los Productos en la BD...

--- php/BajaProd.php
+++ php/BajaProd.php
@@ -49,10 +49,12 @@
                     while($fila = $resultado -> fetch_assoc()){
                         
                         echo '<option value="'.$fila["IDProducto"].'">'.$fila["Nombre"].'</option>';
+                        
                     }
                 ?>
                    
             </select>
+            
             
             <br><br>
             
@@ -66,7 +68,7 @@
     </form>
 </div>
 
-<?php
+<?php       
     
         }
         
@@ -99,5 +101,86 @@
     </style>
 </head>
 <body>
+ <?php
+    
+     $sql = 'select * from productos';//HACEMOS CADENA CON LA SENTENCIA DE MYSQL...
+        $resultado = $conexion -> query($sql); // SENTENCIA...
+        
+// CREACION DE TABLA CON ECHO Y HTML ---------------------------------------------------------------------------------------------
+        
+        // SI HAY UN VALOR O PACIENTE EN EL RENGLON...
+
+        if ($resultado -> num_rows){
+            
+            echo '
+                </head>
+                
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+                </head>';
+                
+            echo '<div>';
+            echo '<table class="table table-hover" style="width:100%; font-style: italic">';
+              
+            echo '<tr>';
+            
+                    echo '<th style="text-align: center;"> ID PRODUCTO   </th>';
+                    echo '<th> NOMBRE          </th>';
+                    echo '<th> CATEGORIA       </th>';
+                    echo '<th> DESCRIPCION     </th>';
+                    echo '<th> EXISTENCIA      </th>';
+                    echo '<th> PRECIO          </th>';
+                    echo '<th> IMAGEN          </th>';
+            
+            echo '</tr>';
+            
+            // IMPRIME LOS VALORES QUE TIENE EL ARREGLO DE FILA...
+            
+            while( $fila = $resultado -> fetch_assoc()){
+                
+                echo '<tr>';
+                
+                    echo '<td style="text-align: center; color: blue;">'. $fila['IDProducto'] . '</td>';
+                    echo '<td>'. $fila['Nombre'] . '</td>';
+                    echo '<td>'. $fila['Categoria'] . '</td>';
+                    echo '<td>'. $fila['Descripcion'] . '</td>';
+                    echo '<td>'. $fila['Existencia'] . '</td>';
+                    echo '<td>'. $fila['Precio'] . '</td>';
+                    echo '<td> <img src="Imagenes/'. $fila['Imagen'] . '" height="75px" width="75px"></td>';
+                
+                echo '</tr>';
+                
+            }   
+            
+            echo '</table">';
+            
+        echo '</div>';
+        
+        }
+        
+         else{
+             
+             echo "no hay datos";
+         }
+        
+        echo '<button><a href="AltaProd.php">Ingresar Datos</a></button>';
+        
+        echo '<br><br>';
+    
+    ?>
+
+    <select   class="custom-select">
+                
+                <?php
+                 
+                $salida='<table>';
+                 
+                while( $fila = $resultado -> fetch_assoc() ){ //recorremos los registros obtenidos de la tabla
+                    
+                    echo '<option value="'.$fila["IDProducto"].'">'.$fila["Nombre"].'</option>';
+                    //proceso de concatenacion de datos
+                }
+                    ?>
+    </select>
 </body>
 </html>
