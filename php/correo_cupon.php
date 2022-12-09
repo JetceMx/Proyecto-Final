7801ffcc56d624797686dab2e56eb16258bfd840
Juan Teran <sebasterra69@gmail.com>
2022-12-08 18:48:47 -0600

    cupon vol. 3

--- php/correo_cupon.php
+++ php/correo_cupon.php
@@ -4,8 +4,8 @@
 $mail = $_POST['cupon'];
 $asunto = 'Sabinito Games';
 $men="<img src='../images/cupon.png'>";
-$header = "Ten un cupon por suscribirte!!!!!"."\r\n";
-mail($mail,$asunto,$men,$header);
+
+mail($mail,$asunto,$men);
 ?>
 <script>
     
