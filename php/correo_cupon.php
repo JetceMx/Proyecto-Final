b11149c69fb90341695242c5b404a478ce840403
Juan Teran <sebasterra69@gmail.com>
2022-12-08 19:31:00 -0600

    cupon vol. 8

--- php/correo_cupon.php
+++ php/correo_cupon.php
@@ -3,7 +3,13 @@
 
 $mail = $_POST['cupon'];
 $asunto = 'Sabinito Games';
-$men="Hola gracias por suscribirte aqui tienes un cupon!!! <br><img src='https://ibb.co/f8D3PC6'> ";
+$men="<html>
+      <body>
+      <p>Gracias por suscribirte ten un cupon!!!</p>
+      <br>
+      <img src='https://ibb.co/f8D3PC6'> 
+      </body>
+      </html>";
 
 mail($mail,$asunto,$men);
 ?>
