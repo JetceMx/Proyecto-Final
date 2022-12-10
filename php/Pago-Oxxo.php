3a5b31ba5e5d673b8f139a3d976cdabcada0b16f
Jetce <116926177+JetceMx@users.noreply.github.com>
2022-12-09 23:29:18 -0600

    Pagina de Pagar en Oxxo terminada

---
+++ php/Pago-Oxxo.php
@@ -0,0 +1,86 @@
+<?php
+include "header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Style-Global.css">
+    <link rel="stylesheet" href="../css/Style-Pago_Oxxo.css">
+    <script src="https://kit.fontawesome.com/3d7b0db529.js" crossorigin="anonymous"></script>
+    <title>Pago-Oxxo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
+
+<body>
+
+    <div class="Contenedor">
+       
+       <div class="Cuadro-1">
+           
+           <div class="Contenido">
+              
+              <div class="Marco">
+                 
+                  <h3>Ficha Digital, no es Necesario Inprimir.</h3>
+                  
+              </div>
+              
+              <br>
+              
+              <div class="Parte-1">
+                 
+                  <img src="../images/Oxxo.png" width="130"  height="120">
+                  
+                  <div class="Parte-1_Derecha">
+                      
+                      <h2>Monto a pagar</h2>
+                      
+                      <h1>$150.00 mxn</h1>
+                      
+<pre>Oxxo Cobrara una comision adicional 
+   al momento de realizar el pago </pre>
+                      
+                  </div>
+                  
+                  
+               </div>
+               
+               <div class="Parte-2">
+                   
+                   <h2>Referencia</h2>
+                   
+                   <div class="Parte-2_Cuadro">
+                      
+                      <h1>4950-9402-3940-87</h1>
+                       
+                       
+                   </div>
+                   
+               </div>
+               
+               <br>
+               
+               <img src="../images/Codigo.png" width="300" height="130" alt="">
+               
+           </div>
+           
+           
+       </div>
+       
+        
+    </div>
+
+
+
+    <?php
+    include "footer.php";
+    ?>
+
+</body>
+
+</html>
